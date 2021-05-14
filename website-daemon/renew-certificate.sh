c234477757515fee44a3b6420a1449b55e58b96c
Nobelle Tay <nobellet@rice11.stanford.edu>
2021-05-14 16:31:52 -0700

    Add renew-certificate.sh

---
+++ website-daemon/renew-certificate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+short_lived_certificate_path="https://www.ieee-security.org/TC/W2SP/2012/papers/w2sp12-final9.pdf"
+
+# Download short-lived certificate and Proof-of-Inclusion for the Merkle tree
+wget $short_lived_certificate_path
+
+# Update certificate path in web server config
+# Skipped since building a web server is out of the scope of this project
+
+# Reload the web server to pick up the new config
+# Skipped since building a web server is out of the scope of this project
