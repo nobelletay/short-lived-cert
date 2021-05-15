c9bd6b3969c6a87046e72e1e6d32e9accc4dd1e7
Nobelle Tay <nobelletay@hotmail.com>
2021-05-14 19:24:08 -0700

    Get optional arguments for cert input and output path

--- website-daemon/renew-certificate.sh
+++ website-daemon/renew-certificate.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-certificate_download_path="https://www.ieee-security.org/TC/W2SP/2012/papers/w2sp12-final9.pdf"
-certificate_path="/home/nobellet/short-lived-cert/website-daemon/certificate.pdf"
+cert_input_path="https://www.ieee-security.org/TC/W2SP/2012/papers/w2sp12-final9.pdf"
+cert_output_path="/home/nobellet/short-lived-cert/website-daemon/certificate.pdf"
+
+print_usage() {
+    printf "Usage: $0 [-i input] [-o output]\n"
+}
+
+while getopts 'i:o:' flag; do
+    case "${flag}" in
+        i) cert_input_path="${OPTARG}" ;;
+        o) cert_output_path="${OPTARG}" ;;
+        *) print_usage
+           exit 1 ;;
+    esac
+done
 
 # Download short-lived certificate and Proof-of-Inclusion for the Merkle tree
-wget $certificate_download_path -O $certificate_path
+wget $cert_input_path -O $cert_output_path
 
 # Update certificate path in web server config
 # Skipped since building a web server is out of the scope of this project
