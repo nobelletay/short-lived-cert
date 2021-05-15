2348d8e960e994fd0423752233ea2c0d4ab75299
Nobelle Tay <nobellet@rice11.stanford.edu>
2021-05-14 18:34:38 -0700

    Add certificate output path

--- website-daemon/renew-certificate.sh
+++ website-daemon/renew-certificate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-short_lived_certificate_path="https://www.ieee-security.org/TC/W2SP/2012/papers/w2sp12-final9.pdf"
+certificate_download_path="https://www.ieee-security.org/TC/W2SP/2012/papers/w2sp12-final9.pdf"
+certificate_path="/home/nobellet/afs-home/short-lived-cert/website-daemon/certificate.pdf"
 
 # Download short-lived certificate and Proof-of-Inclusion for the Merkle tree
-wget $short_lived_certificate_path
+wget $certificate_download_path -O $certificate_path
 
 # Update certificate path in web server config
 # Skipped since building a web server is out of the scope of this project
